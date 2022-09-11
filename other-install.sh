a93b040ec70ca674878532536dc23e3b4b56a74a
Giorgio Salvemini <56552241+Giorgio68@users.noreply.github.com>
2022-09-11 14:03:15 +0200

    Update other-install.sh

--- other-install.sh
+++ other-install.sh
@@ -66,7 +66,6 @@ error "Couldn't update apt!"
 # Install all dependencies
 log "Installing dependencies"
 sudo apt-get install -y libboost-all-dev libusb-1.0.0-dev libssl-dev cmake libprotobuf-dev protobuf-c-compiler protobuf-compiler libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediawidgets5 qtmultimedia5-dev libqt5bluetooth5 libqt5bluetooth5-bin qtconnectivity5-dev pulseaudio librtaudio-dev gcc g++ make perl
-libraspberrypi-doc libraspberrypi-dev
 error "Couldn't install all dependencies! Is Raspbian up to date?"
 
 # Clone AASDK
